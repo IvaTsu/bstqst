0b7bbbfc7c0d5972a5c2de4f4c9813e81665c796
Andrew Marushko <andrewmarushko@gmail.com>
2018-07-30 20:16:53 +0300

    configure routers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppRoutes from "./router";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 registerServiceWorker();
